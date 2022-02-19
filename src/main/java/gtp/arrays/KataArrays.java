e036d017c468bf0a52c1a61c338bf4cdff336e79
unknown <gtcodesmith@gmail.com>
2022-02-19 13:29:18 -0700

    switched to parameterrized tests

--- src/main/java/gtp/arrays/KataArrays.java
+++ src/main/java/gtp/arrays/KataArrays.java
@@ -1,36 +1,43 @@
 package gtp.arrays;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
+import java.util.Stack;
 
 public class KataArrays {
 
     public static int[][] findArrays(int[] nums, int sum) {
 
-        ArrayList<int[]> lst = new ArrayList<>();
+        Stack<int[]> lst = new Stack<>();
 
         ArrayList<Integer> tmp = new ArrayList<>();
 
         if (nums.length > 0) {
             for (int i = 0; i < nums.length; i++) {
                 if (nums[i] == sum) {
-                    lst.add(new int[]{nums[i]});
+                    lst.push(new int[]{nums[i]});
                 }
-                if(nums[i]<sum){
+                if (nums[i] < sum) {
                     tmp.add(nums[i]);
                     int tot = sumTmp(tmp);
 
-                    if(tot==sum || tot==0){
-                        lst.add(tmp.stream().mapToInt(Integer::intValue).toArray());
+                    if (tot == sum || tot == 0) {
+                        int arr[]=tmp.stream().mapToInt(Integer::intValue).toArray();
+                        if(tot == 0)
+                        {
+                           int pre[]= lst.peek();//lst.get(lst.size()-1);
+                           int smash[] = mash(pre,arr);
+                           lst.push(smash);
+
+                        }else {
+                            lst.add(arr);
+                        }
                         tmp.clear();
                     }
                 }
             }
 
             return lst.toArray(new int[][]{});
-        }
-        else
+        } else
             return new int[][]{};
     }
 
@@ -39,9 +46,9 @@ public class KataArrays {
     }
 
     public static int[] mash(int[] arr1, int[] arr2) {
-        int mashed[] = new int[arr1.length+arr2.length];
-        System.arraycopy(arr1,0,mashed,0,arr1.length);
-        System.arraycopy(arr2,0,mashed,arr1.length,arr2.length);
+        int mashed[] = new int[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, mashed, 0, arr1.length);
+        System.arraycopy(arr2, 0, mashed, arr1.length, arr2.length);
         return mashed;
     }
 }
--- src/test/java/KataArraysTest.java
+++ src/test/java/KataArraysTest.java
@@ -1,49 +1,67 @@
 import gtp.arrays.KataArrays;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static gtp.arrays.KataArrays.findArrays;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class KataArraysTest {
 
     @Test
-    public void testFindSubArrays(){
-
+    public void testFindSubArrays() {
         int nums[] = new int[]{10};
-        assertNotNull( findArrays(nums,10));
-        assertTrue(findArrays(nums,10).length == 1);
-        assertArrayEquals(new int[][]{{10}}, findArrays(nums,10));
-
-        int nums2[] = new int[]{4};
-        assertArrayEquals(new int[][]{}, findArrays(nums2,10));
-
-        int nums3[] = new int[]{10,10};
-        assertArrayEquals(new int[][]{{10},{10}}, findArrays(nums3,10));
-
-        int nums4[] = new int[]{6,4};
-        assertArrayEquals(new int[][]{{6,4}}, findArrays(nums4,10));
-
-        int nums5[] = new int[]{6,4,3,7};
-        assertArrayEquals(new int[][]{{6,4},{3,7}}, findArrays(nums5,10));
-
-        int nums6[] = new int[]{10,0};
-        assertEquals(2, findArrays(nums6,10).length);
-
+        assertNotNull(findArrays(nums, 10));
+        assertTrue(findArrays(nums, 10).length == 1);
     }
 
     @Test
-    public void testArrayMash(){
-        int arr1[] = {1,2,3};
-        int arr2[] = {4,5,6};
+    public void testArrayMash() {
+        int arr1[] = {1, 2, 3};
+        int arr2[] = {4, 5, 6};
 
-        assertArrayEquals(new int[]{1,2,3,4,5,6}, KataArrays.mash(arr1,arr2));
-//        assertArrayEquals(new int[]{1,2,3,4,5,6}, KataArrays.mash(null,arr2));
-
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, KataArrays.mash(arr1, arr2));
     }
 
+    @ParameterizedTest
+    @MethodSource("srcArr_Sum_ExpectedArr")
+    void testParameterizedFindArrays(int src[], int sum, int expected[][]) {
+        assertArrayEquals(expected, findArrays(src, sum));
+    }
 
-
-
+    static Stream<Arguments> srcArr_Sum_ExpectedArr() {
+        //3 params 1- src Array  2- Sum  3-expected Array of sub Arrays
+        return Stream.of(
+                arguments(
+                        new int[]{10}, 10,
+                        new int[][]{{10}}
+                ),
+                arguments(
+                        new int[]{4}, 10,
+                        new int[][]{}
+                ),
+                arguments(
+                        new int[]{10, 10}, 10,
+                        new int[][]{{10}, {10}}
+                ),
+                arguments(
+                        new int[]{6, 4, 3, 7}, 10,
+                        new int[][]{{6, 4}, {3, 7}}
+                ),
+                arguments(
+                        new int[]{10, 0}, 10,
+                        new int[][]{{10}, {10, 0}}
+                ),
+                arguments(
+                        new int[]{10, 0, 10}, 10,
+                        new int[][]{{10}, {10, 0}, {0, 10}, {10}}
+                        )
+        );
+    }
 
 
 }
