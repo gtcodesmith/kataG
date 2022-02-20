deb335b7b41cb673e03c33a8cc846e52ffffa0c7
georgetp <g@g.com>
2022-02-20 06:42:12 -0700

    Make functional

--- src/main/java/gtp/arrays/KataArrays.java
+++ src/main/java/gtp/arrays/KataArrays.java
@@ -3,50 +3,52 @@ package gtp.arrays;
 import java.util.ArrayList;
 import java.util.Stack;
 
+import static java.util.Optional.of;
+
 public class KataArrays {
 
     public static int[][] findArrays(int[] nums, int sum) {
 
+        if (nums.length == 0) {
+            return new int[][]{};
+        }
+
         Stack<int[]> subArrays = new Stack<>();
         Stack<int[]> zeroArrays = new Stack<>();
-
         ArrayList<Integer> accum = new ArrayList<>();
 
-        if (nums.length > 0) {
-            int tot=0;
-            for (int num : nums) {
+        int tot = 0;
+        for (int num : nums) {
 
-                accum.add(num);
-                tot += num;
+            accum.add(num);
+            tot += num;
 
-                if (tot == sum || tot == 0) {
-                    int[] arr = accum.stream().mapToInt(Integer::intValue).toArray();
-                    if (tot == 0) {
-                        zeroArrays.push(arr);
-                        if (subArrays.size() > 0) {
-                            int[] pre = subArrays.peek();
-                            int[] smash = mash(pre, arr);
-                            subArrays.push(smash);
-                        }
+            if (tot == sum || tot == 0) {
+                int[] arr = accum.stream().mapToInt(Integer::intValue).toArray();
+                if (tot == 0) {
+                    zeroArrays.push(arr);
+                    of(subArrays)
+                            .filter(sa -> !sa.isEmpty())
+                            .map(Stack::peek)
+                            .map(prevArr -> mash(prevArr, arr))
+                            .ifPresent(subArrays::push);
 
-                    } else {
-                        if (zeroArrays.size() > 0) {
-                            for (int[] zarr : zeroArrays) {
-                                int[] smash = mash(zarr, arr);
-                                subArrays.push(smash);
-                            }
-                            zeroArrays.clear();
-                        }
-                        subArrays.add(arr);
-                    }
+                } else {
+                    zeroArrays
+                            .stream()
+                            .map(zarr -> mash(zarr, arr))
+                            .forEach(subArrays::push);
 
-                    accum.clear();
-                    tot = 0;
+                    zeroArrays.clear();
+                    subArrays.add(arr);
                 }
+
+                accum.clear();
+                tot = 0;
             }
-            return subArrays.toArray(new int[][]{});
-        } else
-            return new int[][]{};
+        }
+
+        return subArrays.toArray(new int[][]{});
     }
 
     public static int[] mash(int[] arr1, int[] arr2) {
